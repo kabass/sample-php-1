7248119dd06fff7b47de7ae596d0845cd28e21c2
bassirou <ka.bassirou@gmail.com>
2017-09-24 08:16:22 +0200

    ddd

--- script.sh
+++ script.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
 
-git checkout -b 1.0 origin/1.0
-
-# geting current version
-IN=$(jq .version composer.json)
-set -- "$IN"
-IFS="."; declare -a Array=($*)
-IN="${Array[2]}"
-set -- "$IN"
-IFS="-";
-declare -a Array=($*)
-declare -a  NVERSION=${Array[0]}
-NPLUSVERSION=$((${NVERSION}+1))
-
-#taging versio
-git tag -a "1.0.${NVERSION}" -m "taging version"
-git push --tags
-
-
-jq .version="\"1.0.${NPLUSVERSION}-dev\"" composer.json | sponge composer.json
-git add .
-git commit -m "jenkins : adding version"
-git push origin 1.0
-git checkout master
-git rebase origin/master
+branches=()
+eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
+for branch in "${branches[@]}"; do
+    echo branch
+done
